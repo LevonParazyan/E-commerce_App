016c9ca81ddc693ea8ad1eeca831cddbc1cca15d
Karen Barseghyan <87120803+KarenBarseghya@users.noreply.github.com>
2023-04-04 04:04:35 +0400

    Add files via upload

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,20 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AuthService } from './auth.service';
+
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authService: AuthService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: any) {
+    const user = await this.userService.findUserById(payload.id);
+    return user;
+  }
+}
